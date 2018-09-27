9ad96810b64d07ab465e95345af0199eb9390c65
tganyan <tganyan@gmail.com>
2018-09-26 19:38:24 -0700

    Adding updated test files, router, server, mountain-mocks, updated readme

--- src/__test__/mountain-router.test.js
+++ src/__test__/mountain-router.test.js
@@ -1,33 +1,35 @@
 'use strict';
 
-process.env.PORT = 3000;
-
 const faker = require('faker');
 const superagent = require('superagent');
 const server = require('../lib/server');
+const mountainMock = require('./lib/mountain-mock');
 
 const API_URL = `http://localhost:${process.env.PORT}/api/mountains`;
 
 describe('api/mountains', () => {
   beforeAll(server.start);
   afterAll(server.stop);
+  beforeEach(mountainMock.p_CleanMountainMocks);
 
   describe('testing post request', () => {
     test('should respond with 200 status code and a new json mountain', () => {
+      const originalRequest = {
+        name: faker.lorem.words(2),
+        elevation: faker.random.number(),
+      };
       return superagent.post(API_URL)
         .set('Content-Type', 'application/json')
-        .send({
-          name: 'Pilchuck',
-          elevation: '5,341′',
-        })
+        .send(originalRequest)
         .then((response) => {
           expect(response.status).toEqual(200);
-          expect(response.body.name).toEqual('Pilchuck');
-          expect(response.body.elevation).toEqual('5,341′');
-          expect(response.body.id).toBeTruthy();
+          expect(response.body.name).toEqual(originalRequest.name);
+          expect(response.body.elevation).toEqual(originalRequest.elevation);
+          expect(response.body._id.toString()).toBeTruthy();
           expect(response.body.timestamp).toBeTruthy();
         });
     });
+
     test('should respond with 400 status code if there is no name', () => {
       return superagent.post(API_URL)
         .set('Content-Type', 'application/json')
@@ -43,64 +45,69 @@ describe('api/mountains', () => {
 
   describe('testing put request', () => {
     test('should respond with 200 status code and updated json mountain', () => {
-      const originalRequest = {
-        name: faker.lorem.words(2),
-        elevation: faker.random.number(),
-      };
-      return superagent.post(API_URL)
-        .set('Content-Type', 'application/json')
-        .send(originalRequest)
-        .then((postResponse) => {
-          originalRequest.id = postResponse.body.id;
-          return superagent.put(`${API_URL}/${postResponse.body.id}`)
+      let savedMountainMock = null;
+      return mountainMock.p_CreateMountainMock()
+        .then((createdMountainMock) => {
+          savedMountainMock = createdMountainMock;
+          return superagent.put(`${API_URL}/${createdMountainMock._id}`)
             .send({
               name: 'Pilchuck YO!',
             });
         })
         .then((getResponse) => {
           expect(getResponse.body.name).toEqual('Pilchuck YO!');
-          expect(getResponse.body.elevation).toEqual(originalRequest.elevation);
+          expect(getResponse.body.elevation).toEqual(savedMountainMock.elevation);
         });
     });
   });
 
   describe('testing get methods', () => {
     test('should respond with 200 status code if there is a matching name', () => {
-      const originalRequest = {
-        name: faker.lorem.words(2),
-        elevation: faker.random.number(),
-      };
-      return superagent.post(API_URL)
-        .set('Content-Type', 'application/json')
-        .send(originalRequest)
-        .then((postResponse) => {
-          originalRequest.id = postResponse.body.id;
-          return superagent.get(`${API_URL}/${postResponse.body.id}`);
+      let savedMountainMock = null;
+      return mountainMock.p_CreateMountainMock()
+        .then((createdMountainMock) => {
+          savedMountainMock = createdMountainMock;
+          return superagent.get(`${API_URL}/${createdMountainMock._id}`);
         })
         .then((getResponse) => {
+          expect(getResponse.status).toEqual(200);
           expect(getResponse.body.timestamp).toBeTruthy();
-          expect(getResponse.body.id).toEqual(originalRequest.id);
-          expect(getResponse.body.name).toEqual(originalRequest.name);
-          expect(getResponse.body.elevation).toEqual(originalRequest.elevation);
+          expect(getResponse.body._id).toEqual(savedMountainMock._id.toString());
+          expect(getResponse.body.name).toEqual(savedMountainMock.name);
+          expect(getResponse.body.elevation).toEqual(savedMountainMock.elevation);
         });
     });
   });
 
   describe('testing delete requests', () => {
     test('should respond with 204 status code', () => {
-      const originalRequest = {
-        name: faker.lorem.words(2),
-        elevation: faker.random.number(),
-      };
-      return superagent.post(API_URL)
-        .set('Content-Type', 'application/json')
-        .send(originalRequest)
-        .then((postResponse) => {
-          originalRequest.id = postResponse.body.id;
-          return superagent.delete(`${API_URL}/${postResponse.body.id}`);
+      return mountainMock.p_CreateMountainMock()
+        .then((createdMountainMock) => {
+          return superagent.delete(`${API_URL}/${createdMountainMock._id}`);
         })
         .then((getResponse) => {
           expect(getResponse.status).toEqual(204);
+        });
+    });
+  });
+
+  describe('testing bad routes', () => {
+    test('should respond with 404 status code', () => {
+      return mountainMock.p_CreateMountainMock()
+        .then(() => {
+          return superagent.get(`${API_URL}/somebadroute`);
+        })
+        .then((getResponse) => {
+          expect(getResponse.status).toEqual(404);
+        });
+    });
+    test('should respond with 400 for valid request with no id', () => {
+      return mountainMock.p_CreateMountainMock()
+        .then(() => {
+          return superagent.get(`${API_URL}/`);
+        })
+        .then((getResponse) => {
+          expect(getResponse.status).toEqual(400);
         });
     });
   });
